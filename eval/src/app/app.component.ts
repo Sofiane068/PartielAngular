27fdd24112101a22dc574208f1318035f4912291
Sofiane Hachemaoui <sofiane068@hotmail.fr>
2022-01-25 10:50:16 +0100

    Exercice 4

--- eval/src/app/app.component.ts
+++ eval/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Statistique } from 'src/models/types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'eval';
+  statFrance: Statistique;
+  statChine: Statistique;
+
+  constructor() {
+    this.statFrance = new Statistique(
+      'fa1f5f40-be3b-11eb-91ec-7f5875ecfb46',
+      'Démographie en France',
+      '60M'
+    );
+    this.statChine = new Statistique(
+      'fa1f5f40-me3k-41ob-91ec-7j8745ecfb46',
+      'Démographie en Chine',
+      '200M'
+    );
+  }
 }
