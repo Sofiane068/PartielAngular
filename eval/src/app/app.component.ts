c4432dc12d9962dba9a42d9a688c49d071321b02
Sofiane Hachemaoui <sofiane068@hotmail.fr>
2022-01-25 11:15:58 +0100

    Exercice 6

--- eval/src/app/app.component.ts
+++ eval/src/app/app.component.ts
@@ -8,5 +8,16 @@ import { Statistique } from 'src/models/types';
 })
 export class AppComponent {
   title = 'eval';
-  
+  listStats: Statistique[] = [
+    new Statistique(
+      'fa1f5f40-be3b-11eb-91ec-7f5875ecfb46',
+      'Démographie en France',
+      '60M'
+    ),
+     new Statistique(
+      'fa1f5f40-me3k-41ob-91ec-7j8745ecfb46',
+      'Démographie en Chine',
+      '200M'
+    )
+  ];
 }
--- eval/src/app/statistique/statistique.component.ts
+++ eval/src/app/statistique/statistique.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Statistique } from 'src/models/types';
 
 @Component({
@@ -7,20 +7,10 @@ import { Statistique } from 'src/models/types';
   styleUrls: ['./statistique.component.css'],
 })
 export class StatistiqueComponent implements OnInit {
-  statFrance: Statistique;
-  statChine: Statistique;
+ @Input() stat!: Statistique;
 
   constructor() {
-    this.statFrance = new Statistique(
-      'fa1f5f40-be3b-11eb-91ec-7f5875ecfb46',
-      'Démographie en France',
-      '60M'
-    );
-    this.statChine = new Statistique(
-      'fa1f5f40-me3k-41ob-91ec-7j8745ecfb46',
-      'Démographie en Chine',
-      '200M'
-    );
+    
   }
 
   ngOnInit(): void {}
