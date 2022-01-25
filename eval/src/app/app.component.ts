4899d5e3a53c8cb2b16a5fa9e56d0573baf3fdc6
Sofiane Hachemaoui <sofiane068@hotmail.fr>
2022-01-25 11:56:46 +0100

    Exercice 7

--- eval/src/app/app.component.ts
+++ eval/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Statistique } from 'src/models/types';
+import { StatistiqueService } from 'src/service/statistique.service';
 
 @Component({
   selector: 'app-root',
@@ -8,33 +9,20 @@ import { Statistique } from 'src/models/types';
 })
 export class AppComponent {
   title = 'eval';
-  listStats: Statistique[] = [
-    new Statistique(
-      'fa1f5f40-be3b-11eb-91ec-7f5875ecfb46',
-      'Démographie en France',
-      '60M'
-    ),
-    new Statistique(
-      'fa1f5f40-me3k-41ob-91ec-7j8745ecfb46',
-      'Démographie en Chine',
-      '200M'
-    ),]
+  tabStats: Statistique[] = this.monService.listStats;
 
- constructor(){
-setTimeout(() => {
-  this.listStats.push(
-    new Statistique(
-      'fa1f5f40-me3k-41ob-91ec-7j8745ecfb46',
-      'Démographie en Allemagne',
-      '50M'
-    )
-  );
-}, 3000);
+  constructor(public monService: StatistiqueService) {
 
- ;
- }
-   
-
+    setTimeout(() => {
+      this.tabStats.push(
+        new Statistique(
+          'fa1f5f40-me3k-41ob-91ec-7j8745ecfb46',
+          'Démographie en Allemagne',
+          '50M'
+        )
+      );
+    }, 3000);
+  }
 }
 
 
---
+++ eval/src/service/statistique.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Statistique } from 'src/models/types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StatistiqueService {
+  listStats: Statistique[] = [
+    new Statistique(
+      'fa1f5f40-be3b-11eb-91ec-7f5875ecfb46',
+      'Démographie en France',
+      '60M'
+    ),
+    new Statistique(
+      'fa1f5f40-me3k-41ob-91ec-7j8745ecfb46',
+      'Démographie en Chine',
+      '200M'
+    ),
+  ];
+  constructor() {}
+}
